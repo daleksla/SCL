13b643657f04acbeac7f194b7d9d2b27577d7493
Salih <ahmed233@uni.coventry.ac.uk>
2021-06-28 00:09:41 +0100

    Renamed file - renamed c_vector -> vector, because the c isn't necessary

--- vector.h
+++ vector.h
@@ -1,5 +1,5 @@
-#ifndef C_VECTOR_H
-#define C_VECTOR_H
+#ifndef VECTOR_H
+#define VECTOR_H
 #pragma once
 
 #include <stdlib.h> // malloc, free
@@ -16,39 +16,41 @@ extern "C++" {
 #endif // __cplusplus
 
 typedef struct {
-	/** c_vector struct, contains properties pertaining to vector **/
+	/** vector struct, contains properties pertaining to vector **/
 
 	void* data ; // stores vector data
+	
 	unsigned long bytes ; // stores bytes allocated
+	
 	unsigned long size ; // stores actual items of data
 
-} c_vector ;
+} vector ;
 
-/** c_vector_init function, initialises empty vector
-  * @return initialised c_vector struct **/
-c_vector c_vector_init() ;
+/** vector_init function, initialises empty vector
+  * @return initialised vector struct **/
+vector vector_init() ;
 
-/** c_vector_copy function, copies contents of a given c_vector. 
-  * @param const pointer to c_vector
-  * @return copy of given c_vector **/
-c_vector c_vector_copy(const c_vector*) ;
+/** vector_copy function, copies contents of a given vector. 
+  * @param const pointer to vector
+  * @return copy of given vector **/
+vector vector_copy(const vector*) ;
 
-/** c_vector_fini function, frees data held by provided c_vector
-  * @param pointer to c_vector **/
-void c_vector_fini(c_vector*) ;
+/** vector_fini function, frees data held by provided vector
+  * @param pointer to vector **/
+void vector_fini(vector*) ;
 
-c_vector c_vector_init()
+vector vector_init()
 {
-	c_vector vec ;
+	vector vec ;
 	vec.data = NULL ;
 	vec.bytes = 0 ;
 	vec.size = 0 ;
 	return vec ;
 }
 
-c_vector c_vector_copy(const c_vector* cpVec)
+vector vector_copy(const vector* cpVec)
 {
-	c_vector vec ;
+	vector vec ;
 	vec.bytes = cpVec->bytes ;
 	vec.size = cpVec->size ;
 	if(vec.bytes) 
@@ -62,6 +64,9 @@ c_vector c_vector_copy(const c_vector* cpVec)
 	return vec ;
 }
 
+/** vector_append function, adds item of data to end of a vector data and resizes accordingly
+  * @param pointer to vector to append data to
+  * @param l-value (ie pre-initialised) item of data to add **/
 #define c_vector_append(vec, item) ({\
 	unsigned long bytes = sizeof(item) ;\
 	/* then, allocate more memory accordingly, copy the data over, add new data, delete old location */\
@@ -75,7 +80,7 @@ c_vector c_vector_copy(const c_vector* cpVec)
 	++vec->size ;\
 })
 
-void c_vector_fini(c_vector* vec)
+void vector_fini(vector* vec)
 {
 	if(vec->data != NULL) 
 	{
